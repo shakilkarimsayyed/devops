1dcc86680931db8d071c3b1ed410dec8b10a0199
shakilkarimsayyed <dba.shakil@gmail.com>
2020-05-28 04:03:42 +0000

    changed test to success

--- test.rb
+++ test.rb
@@ -8,7 +8,7 @@ HelloWorld
 end
 def test_my_default
 get '/'
-assert_equal 'Hello, world!!!!!!', last_response.body
+assert_equal 'Hello, world!', last_response.body
 end
 def test_with_params
 get '/Frank'
